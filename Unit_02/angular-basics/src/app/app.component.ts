a5d8e528bb455c15d89d0559217d1c9ace4bf747
Ilya Kovalenko <da_ikovalenko@phreesia.com>
2023-01-17 08:45:58 +0200

    Unit_02 lesson 14

--- Unit_02/angular-basics/src/app/app.component.ts
+++ Unit_02/angular-basics/src/app/app.component.ts
@@ -6,31 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Dynamic title';
-  number = 42;
-  arr = [1,2,3];
-  obj = {a:1, b: {c:2}};
 
-  inputValue = '';
-  // img = 'https://angular.io/assets/images/logos/angularjs/AngularJS-Shield.svg';
+  title = 'initial';
 
-  constructor() {
-    // setTimeout(() => {
-    //   console.log('Timeout is elapsed');
-    //   this.img = 'https://thumbnail.imgbin.com/2/18/6/imgbin-react-javascript-library-angularjs-github-89JYbnwWSANSrkV8tMnFuDALH_t.jpg';
-    // }, 5000);
-  }
-
-  onInput(event: Event) {
-     this.inputValue = (<HTMLInputElement>event?.target).value;
-  }
-
-  onBlur(val: string) {
-    this.inputValue = val;
-  }
-
-
-  onClick() {
-    console.log('Click!');
+  onInput(event: any): void {
+    this.title = event.target.value;
   }
 }
