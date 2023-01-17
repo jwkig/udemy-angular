89695d56d7c270a485627fc75563e2dd19151b62
Ilya Kovalenko <da_ikovalenko@phreesia.com>
2023-01-17 14:32:23 +0200

    Unit_02 lesson 19

--- Unit_02/angular-basics/src/app/app.component.ts
+++ Unit_02/angular-basics/src/app/app.component.ts
@@ -6,6 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  arr: number[] = [1,1,3,5,8,13];
 
-  toggle: any = false;
+  objs = [
+    {title: 'Post 1', author: 'Vladilen', comments: [
+        {name: 'Max', text:'Lorem1'},
+        {name: 'Max', text:'Lorem2'},
+        {name: 'Max', text:'Lorem3'},
+      ]},
+    {title: 'Post 2', author: 'Vladilen 2', comments: [
+        {name: 'Max 2', text:'Lorem1'},
+        {name: 'Max 2', text:'Lorem2'},
+        {name: 'Max 2', text:'Lorem3'},
+      ]}
+
+  ]
 }
