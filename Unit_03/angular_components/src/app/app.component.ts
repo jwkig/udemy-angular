733e23a8ce09c41c02d43f973f7cfb387eb78943
Ilya Kovalenko <da_ikovalenko@phreesia.com>
2023-02-03 09:27:08 +0200

    Unit_03 lesson 22

--- Unit_03/angular_components/src/app/app.component.ts
+++ Unit_03/angular_components/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  posts: Post[] = [
+    {title: 'Хочу выучить Angular компоненты', text: 'Я все ещу учу компоненты', id: 1},
+    {title: 'Следующий блок', text: 'Будет про директивы', id: 2}
+  ]
 
 }
