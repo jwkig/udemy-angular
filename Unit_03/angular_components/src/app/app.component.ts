c91046380ea777afe465b3f66e12c3dd7eaa8ee1
Ilya Kovalenko <da_ikovalenko@phreesia.com>
2023-02-06 15:03:09 +0200

    Unit_03 lesson 27

--- Unit_03/angular_components/src/app/app.component.ts
+++ Unit_03/angular_components/src/app/app.component.ts
@@ -22,4 +22,8 @@ export class AppComponent {
     console.log('Post: ', post);
   }
 
+  removePost(id: number) {
+    console.log('Id to remove ', id);
+    this.posts = this.posts.filter(p => p.id !== id);
+  }
 }
--- Unit_03/angular_components/src/app/post/post.component.ts
+++ Unit_03/angular_components/src/app/post/post.component.ts
@@ -1,4 +1,15 @@
-import {Component, ContentChild, ElementRef, Input, OnInit} from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit, AfterViewChecked, AfterViewInit,
+  Component,
+  ContentChild,
+  DoCheck,
+  ElementRef, EventEmitter,
+  Input,
+  OnChanges, OnDestroy,
+  OnInit, Output,
+  SimpleChanges
+} from '@angular/core';
 import {Post} from "../app.component";
 
 @Component({
@@ -6,15 +17,59 @@ import {Post} from "../app.component";
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent implements OnInit{
+export class PostComponent implements
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+{
 
   @Input() post: Post = {title:'', text:''};
+  @Output() onRemove: EventEmitter<number> = new EventEmitter<number>();
   @ContentChild('info', {static: true}) infoRef: ElementRef = {} as ElementRef;
 
   constructor() {
+    console.log('constructor');
   }
 
-  ngOnInit() {
-    console.log(this.infoRef.nativeElement);
+  removePost() {
+    this.onRemove.emit(this.post.id);
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+    // console.log(this.infoRef.nativeElement);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges', changes);
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
   }
 }
